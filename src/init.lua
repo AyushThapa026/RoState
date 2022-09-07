a93be3ca4b1ad4d971e86ea95f63eaee93058668
AyushThapa026 <ayushthapa026@gmail.com>
2022-09-06 21:03:20 -0700

    changed example code + more fixes

--- src/init.lua
+++ src/init.lua
@@ -1,32 +1,6 @@
-local RunService = game:GetService("RunService")
 --- Manages multiple states via events
 -- Useful for coordinating multiple states
---[[
-    stateData = {
-        initial = "idle", -- // default is "none"; this is the value that is initialized
-        events = {
-            {name = "walk", from = "idle", to = "walking"},
-            {name = "die", from = "any", to = "died"}, -- // a from state of "any" will allow this event to be called from any event
-            {name = "stop", from = "any", to = "idle"}
-        },
-        callback = {
-            {
-                walking_leave = function() print("Stopped walking") end,
-                walk_enter = function() print("Started walking") end,
-
-                die_enter = function() print("Died") end,
-            }
-        }
-    }
-
-    local RoState = require(PATH_HERE)
-    local StateMachine = RoState.new(stateData),
-
-    StateMachine.walk() -- // sets current state to "walking"
-    print(StateMachine:GetState()) -- returns "walking"
---]]
 -- @classmod RoState
-
 local Signal = require(script.Signal)
 local Maid = require(script.Maid)
 local Promise = require(script.Promise)
@@ -42,16 +16,13 @@ function RoState.new(stateData)
     assert(stateData.events, "[RoState.new] events are required to properly initialize RoState")
     local self = setmetatable({
         _initial = stateData.initial or "none";
+
         _state = nil;
         _lastState = nil;
-
         _callbacks = stateData.callbacks or {};
-
-        _queue = {};
-
         _events = {};
         
-        signals = {Entered = Maid.new(), Left = Maid.new()}
+        _signals = {Entered = Maid.new(), Left = Maid.new()}
     }, RoState)
 
     for _, event in pairs(stateData.events) do
@@ -88,15 +59,15 @@ local function getOrCreateSignal(array, index)
 end
 
 function RoState:OnStateLeft(stateName)
-    self.signals.Left[stateName] = getOrCreateSignal(self.signals.Left, stateName);
+    self._signals.Left[stateName] = getOrCreateSignal(self._signals.Left, stateName);
 
-    return self.signals.Left[stateName]
+    return self._signals.Left[stateName]
 end
 
 function RoState:OnStateEnter(stateName)
-    self.signals.Entered[stateName] = getOrCreateSignal(self.signals.Entered, stateName);
+    self._signals.Entered[stateName] = getOrCreateSignal(self._signals.Entered, stateName);
 
-    return self.signals.Entered[stateName]
+    return self._signals.Entered[stateName]
 end
 
 --- Add an event that can change state when called
@@ -110,11 +81,11 @@ function RoState:AddEvent(eventName : string, stateFrom : string, stateTo : stri
                     self._lastState = self._state
                     self._state = stateTo
     
-                    self.signals.Entered[stateTo] = getOrCreateSignal(self.signals.Entered, stateTo);
-                    self.signals.Left[stateFrom] = getOrCreateSignal(self.signals.Left, stateTo);
+                    self._signals.Entered[stateTo] = getOrCreateSignal(self._signals.Entered, stateTo);
+                    self._signals.Left[stateFrom] = getOrCreateSignal(self._signals.Left, stateTo);
     
-                    self.signals.Entered[stateTo]:Fire(self, eventName, stateFrom, stateTo)
-                    self.signals.Left[stateFrom]:Fire(self, eventName, stateFrom, stateTo)
+                    self._signals.Entered[stateTo]:Fire(self, eventName, stateFrom, stateTo)
+                    self._signals.Left[stateFrom]:Fire(self, eventName, stateFrom, stateTo)
     
                     if self._callbacks[self._lastState .. "_leave"] then
                         self._callbacks[self._lastState .. "_leave"](self, eventName, stateFrom, stateTo)
@@ -133,8 +104,8 @@ function RoState:AddEvent(eventName : string, stateFrom : string, stateTo : stri
             end
             
         end
-        self._events["get" .. eventName] = function()
-            return stateFrom, stateTo
+        self._events["get_" .. eventName] = function()
+            return stateTo, stateFrom
         end
     else
         warn(("[RoState.%s] already exists"):format(eventName))
@@ -142,8 +113,8 @@ function RoState:AddEvent(eventName : string, stateFrom : string, stateTo : stri
 end
 
 function RoState:Can(event : string)
-    if self[event] then
-        local _, stateFrom = self["get" .. event]()
+    if self._events[event] then
+        local _, stateFrom = self._events["get_" .. event]()
         if stateFrom == self._state then
             return true
         end
@@ -151,10 +122,23 @@ function RoState:Can(event : string)
     return false
 end
 
+function RoState:GetTransitionMethods(state : string)
+    state = state or self._state
+    local events = {}
+    for event, func in pairs(self._events) do
+        if string.sub(event, 1, 4) == "get_" then
+            local stateTo, stateFrom = func()
+            if (stateFrom == state) or stateFrom == "any" then
+                table.insert(events, string.sub(event, 5, #event))
+            end
+        end
+    end
+    return events
+end
 
 function RoState:Destroy()
-    self.signals.Entered:DoCleaning()
-    self.signals.Left:DoCleaning()
+    self._signals.Entered:DoCleaning()
+    self._signals.Left:DoCleaning()
 
     table.clear(self._callbacks);
     table.clear(self.events);
